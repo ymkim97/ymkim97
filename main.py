1418230f6ade16f054c7fb838fc33cc9f0d6cc15
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2025-09-06 18:27:26 +0900

    Fix variable name from markdown_text to blog_text

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ for idx, feed in enumerate(RSS_FEED['entries']):
         break
     else:
         feed_date = feed['published_parsed']
-        markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
+        blog_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
 
 f.write(markdown_text)
 f.close()
