760b4c2e3817d044f0aedc72362102c50054d10c
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2025-09-06 18:28:40 +0900

    Fix blog_text write operation position

--- main.py
+++ main.py
@@ -117,15 +117,13 @@ blog_text = """
 """ # list of blog posts will be appended here
 
 f = open("README.md",mode="w", encoding="utf-8")
-f.write(blog_text)
-
 for idx, feed in enumerate(RSS_FEED['entries']):
     if idx > MAX_POST:
         break
     else:
         feed_date = feed['published_parsed']
         blog_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
-
+f.write(blog_text)
 f.write(markdown_text)
 f.close()
 
